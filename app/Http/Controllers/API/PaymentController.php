f75268ce53d3ea14a65b84c7b0bb42517fba2521
khaldmansour <khaldmansour93@gmail.com>
2024-12-21 21:10:39 +0200

    [error-handling] handling errors of PaymentController

--- app/Http/Controllers/API/PaymentController.php
+++ app/Http/Controllers/API/PaymentController.php
@@ -7,26 +7,43 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\InitiatePaymentRequest;
 use App\Http\Requests\VerifyPaymentRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class PaymentController extends Controller
 {
     public function initiatePayment(InitiatePaymentRequest $request){
-        $paymentGateway = PaymentGatewayFactory::make($request->input('gateway'));
+        try{
+            $paymentGateway = PaymentGatewayFactory::make($request->input('gateway'));
 
-        return $this->successResponse(['checkout_url' =>$paymentGateway->initiatePayment($request->all())]);
+            return $this->successResponse(['checkout_url' =>$paymentGateway->initiatePayment($request->all())]);
+
+        }catch(\Exception $e){
+            return $this->failureResponse($e->getMessage());
+        }
+
     }
 
     public function verifyPayment(VerifyPaymentRequest $request){
-        $paymentGateway = PaymentGatewayFactory::make($request->input('gateway'));
+        try{
+            $paymentGateway = PaymentGatewayFactory::make($request->input('gateway'));
 
-        return $this->successResponse(['successfulPayment' =>$paymentGateway->verifyPayment($request->input('paymentId'))]);
+            return $this->successResponse(['successfulPayment' =>$paymentGateway->verifyPayment($request->input('paymentId'))]);
+        }catch(\Exception $e){
+            return $this->failureResponse($e->getMessage());
+        }
+
     }
 
     public function handleStripeWebhook(Request $request)
     {
-        $paymentGateway = PaymentGatewayFactory::make('stripe');
-        $paymentGateway->handleWebhook($request);
+        try{
+            $paymentGateway = PaymentGatewayFactory::make('stripe');
+            $paymentGateway->handleWebhook($request);
+            
+            return $this->successResponse(['status' => 'success']);
+        }catch(\Exception $e){
+            Log::error($e->getMessage());   
+        }
         
-        return $this->successResponse(['status' => 'success']);
     }
 }
