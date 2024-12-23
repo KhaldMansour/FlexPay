93b2bc06119b91c944c7fa25d968f3c3c4b7bcd3
khaldmansour <khaldmansour93@gmail.com>
2024-12-23 15:29:54 +0200

    [test] update unit tests

--- tests/Unit/Controllers/PaymentControllerTest.php
+++ tests/Unit/Controllers/PaymentControllerTest.php
@@ -6,6 +6,7 @@ use App\Factories\PaymentGatewayFactory;
 use App\Http\Requests\InitiatePaymentRequest;
 use App\Http\Controllers\API\PaymentController;
 use App\Http\Requests\VerifyPaymentRequest;
+use App\Interfaces\PaymentGatewayInterface;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Mockery;
@@ -13,18 +14,19 @@ use Tests\TestCase;
 
 class PaymentControllerTest extends TestCase
 {
-    public function test_initiatePayment_successs()
+    public function test_initiatePayment_success()
     {
-        $paymentGatewayMock = Mockery::mock();
+        $requestData = [
+            'gateway' => 'stripe',
+            'product_name' => 'Test Product',
+            'amount' => 100,
+            'quantity' => 1,
+            'currency' => 'USD'
+        ];
+        $paymentGatewayMock = Mockery::mock(PaymentGatewayInterface::class);
         $paymentGatewayMock->shouldReceive('initiatePayment')
             ->once()
-            ->with([
-                'gateway' => 'stripe',
-                'product_name' => 'Test Product',
-                'amount' => 100,
-                'quantity' => 1,
-                'currency' => 'USD'
-            ])
+            ->with($requestData)
             ->andReturn('https://checkout.stripe.com/');
         $paymentGatewayFactoryMock = Mockery::mock('alias:' . PaymentGatewayFactory::class);
         $paymentGatewayFactoryMock->shouldReceive('make')
@@ -32,51 +34,47 @@ class PaymentControllerTest extends TestCase
             ->andReturn($paymentGatewayMock);
         $requestMock = Mockery::mock(InitiatePaymentRequest::class);
         $requestMock->shouldReceive('input')->with('gateway')->andReturn('stripe');
-        $requestMock->shouldReceive('all')->andReturn([
-            'gateway' => 'stripe',
-            'product_name' => 'Test Product',
-            'amount' => 100,
-            'quantity' => 1,
-            'currency' => 'USD'
-        ]);
-        $controller = new PaymentController($paymentGatewayFactoryMock);
+        $requestMock->shouldReceive('all')->andReturn($requestData);
+        $controller = new PaymentController();
 
         $response = $controller->initiatePayment($requestMock);
 
         $this->assertEquals(Response::HTTP_OK, $response->status());
         $this->assertEquals('https://checkout.stripe.com/', $response->getData()->data->checkout_url);
     }
-    
+
     public function test_initiatePayment_failure()
     {
-        $paymentGatewayMock = Mockery::mock();
+        $requestData = [
+            'gateway' => 'stripe',
+            'product_name' => 'Test Product',
+            'amount' => 100,
+            'quantity' => 1,
+            'currency' => 'USD'
+        ];
+        $paymentGatewayMock = Mockery::mock(PaymentGatewayInterface::class);
         $paymentGatewayMock->shouldReceive('initiatePayment')
             ->once()
-            ->andThrow(new \Exception('Payment gateway error'));
+            ->with($requestData)
+            ->andThrow(new \Exception('Payment gateway error'));    
         $paymentGatewayFactoryMock = Mockery::mock('alias:' . PaymentGatewayFactory::class);
         $paymentGatewayFactoryMock->shouldReceive('make')
             ->with('stripe')
             ->andReturn($paymentGatewayMock);
         $requestMock = Mockery::mock(InitiatePaymentRequest::class);
         $requestMock->shouldReceive('input')->with('gateway')->andReturn('stripe');
-        $requestMock->shouldReceive('all')->andReturn([
-            'gateway' => 'stripe',
-            'product_name' => 'Test Product',
-            'amount' => 100,
-            'quantity' => 1,
-            'currency' => 'USD'
-        ]);
-        $controller = new PaymentController($paymentGatewayFactoryMock);
-
+        $requestMock->shouldReceive('all')->andReturn($requestData);
+        $controller = new PaymentController();
+    
         $response = $controller->initiatePayment($requestMock);
-
+    
         $this->assertEquals(Response::HTTP_BAD_REQUEST, $response->status());
         $this->assertEquals('Payment gateway error', $response->getData()->message);
     }
-
+    
     public function test_verifyPayment_success()
     {
-        $paymentGatewayMock = Mockery::mock();
+        $paymentGatewayMock = Mockery::mock(PaymentGatewayInterface::class);
         $paymentGatewayMock->shouldReceive('verifyPayment')
             ->once()
             ->with('paymentId')
@@ -84,25 +82,26 @@ class PaymentControllerTest extends TestCase
         $paymentGatewayFactoryMock = Mockery::mock('alias:' . PaymentGatewayFactory::class);
         $paymentGatewayFactoryMock->shouldReceive('make')
             ->with('stripe')
-            ->andReturn($paymentGatewayMock);
+            ->andReturn($paymentGatewayMock);    
         $requestMock = Mockery::mock(VerifyPaymentRequest::class);
-        $requestMock->shouldReceive('input')->with('gateway')->andReturn('stripe');
         $requestMock->shouldReceive('input')->with('paymentId')->andReturn('paymentId');
-        $controller = new PaymentController($paymentGatewayFactoryMock);
-
+        $requestMock->shouldReceive('input')->with('gateway')->andReturn('stripe');
+        $controller = new PaymentController();
+    
         $response = $controller->verifyPayment($requestMock);
-
+    
         $this->assertEquals(Response::HTTP_OK, $response->status());
         $this->assertTrue($response->getData()->data->successfulPayment);
     }
+    
 
     public function test_verifyPayment_failure()
     {
-        $paymentGatewayMock = Mockery::mock();
+        $paymentGatewayMock = Mockery::mock(PaymentGatewayInterface::class);
         $paymentGatewayMock->shouldReceive('verifyPayment')
             ->once()
             ->with('invalid_payment_id')
-            ->andThrow(new \Exception('Verification failed'));
+            ->andThrow(new \Exception('Verification failed'));    
         $paymentGatewayFactoryMock = Mockery::mock('alias:' . PaymentGatewayFactory::class);
         $paymentGatewayFactoryMock->shouldReceive('make')
             ->with('stripe')
@@ -111,32 +110,56 @@ class PaymentControllerTest extends TestCase
         $requestMock->shouldReceive('input')->with('paymentId')->andReturn('invalid_payment_id');
         $requestMock->shouldReceive('input')->with('gateway')->andReturn('stripe');
         $controller = new PaymentController();
-
+    
         $response = $controller->verifyPayment($requestMock);
-
+    
         $this->assertEquals(Response::HTTP_BAD_REQUEST, $response->status());
         $this->assertEquals('Verification failed', $response->getData()->message);
     }
+    
 
     public function test_handleStripeWebhook_success()
     {
-        $paymentGatewayMock = Mockery::mock();
+        $paymentGatewayMock = Mockery::mock(PaymentGatewayInterface::class);
         $paymentGatewayMock->shouldReceive('handleWebhook')
             ->once()
             ->with(Mockery::type(Request::class))
-            ->andReturn(true);
+            ->andReturn(true);     
         $paymentGatewayFactoryMock = Mockery::mock('alias:' . PaymentGatewayFactory::class);
         $paymentGatewayFactoryMock->shouldReceive('make')
             ->with('stripe')
             ->andReturn($paymentGatewayMock);
         $requestMock = Mockery::mock(Request::class);
-
+        $requestMock->shouldReceive('input')->with('paymentId')->andReturn('invalid_payment_id');
+        $requestMock->shouldReceive('input')->with('gateway')->andReturn('stripe');
         $controller = new PaymentController();
-
+    
         $response = $controller->handleStripeWebhook($requestMock);
-
+    
         $this->assertEquals(Response::HTTP_OK, $response->status());
-        $this->assertEquals('success', $response->getData()->data->status);
+    }
+
+    public function test_handleStripeWebhook_failure()
+    {
+        $paymentGatewayMock = Mockery::mock(PaymentGatewayInterface::class);
+        $paymentGatewayMock->shouldReceive('handleWebhook')
+            ->once()
+            ->with(Mockery::type(Request::class))
+            ->andThrow(new \Exception('Websocket failed'));    
+        $paymentGatewayFactoryMock = Mockery::mock('alias:' . PaymentGatewayFactory::class);
+        $paymentGatewayFactoryMock->shouldReceive('make')
+            ->with('stripe')
+            ->andReturn($paymentGatewayMock);
+        $requestMock = Mockery::mock(Request::class);
+        $requestMock->shouldReceive('input')->with('paymentId')->andReturn('invalid_payment_id');
+        $requestMock->shouldReceive('input')->with('gateway')->andReturn('stripe');
+        $controller = new PaymentController();
+    
+        $response = $controller->handleStripeWebhook($requestMock);
+    
+        $this->assertEquals(Response::HTTP_BAD_REQUEST, $response->status());
+        $this->assertEquals('Websocket failed', $response->getData()->message);
+
     }
 
     public function tearDown(): void
